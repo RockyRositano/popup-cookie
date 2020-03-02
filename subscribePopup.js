a3a085d361f19ffc516c738ece14becd5650755f
Rocky <rockyrositano@outlook.com>
2020-03-03 09:46:47 +1030

    Add files via upload

---
+++ subscribePopup.js
@@ -0,0 +1,39 @@
+
+export function subscribePopup(){
+
+    function setCookie( c_name, value, exdays ) {
+        var c_value = escape(value);
+        if (exdays) {
+            var exdate = new Date();
+            exdate.setDate( exdate.getDate() + exdays );
+            c_value += '; expires=' + exdate.toUTCString();
+        }
+        document.cookie = c_name + '=' + c_value;
+    }
+    
+    function getCookie( c_name ) {
+        var i, x, y, cookies = document.cookie.split( ';' );
+    
+        for ( i = 0; i < cookies.length; i++ ) {
+            x = cookies[i].substr( 0, cookies[i].indexOf( '=') );
+            y = cookies[i].substr( cookies[i].indexOf( '=') + 1 );
+            x = x.replace( /^\s+|\s+$/g, '' );
+    
+            if ( x === c_name ) {
+                return unescape( y );
+            }
+        }
+    }
+
+    window.setTimeout(function(){ 
+        // When the cookie exists, do not show the popup!
+        if (getCookie('displayedPopupForm')) {
+            return;
+        }
+    
+        $.fancybox.open($('#subscribe'));
+    
+        // The popup was displayed. Set the cookie for 1 day.
+        setCookie('displayedPopupForm', 'yes', 1);
+    }, 3000);
+}
